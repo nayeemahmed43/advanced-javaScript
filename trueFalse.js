17474e26fa7a9dffb68aee75da21901734bdd03d
unknown <you@example.com>
2020-03-04 12:07:31 +0600

    truthy false

---
+++ trueFalse.js
@@ -0,0 +1,17 @@
+
+// falsy values
+// zero 
+// empty string 
+// null 
+// NaN 
+// undefined 
+// false 
+
+
+//const age = 4;
+let name;
+if(name){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
